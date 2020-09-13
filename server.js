b263c6ff299b41efe0463febb9728b2ea32860e1
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-09-13 23:03:11 +0430

    add Middleware

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Middleware
+const cors = require("cors");
+const helmet = require("helmet");
+app.use(cors);
+app.use(helmet);
+app.use(express.json());
+require("dotenv").config();
 
 // Running Server
 const PORT = process.env.PORT || 5000;
