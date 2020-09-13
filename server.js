b2097339a229d9b69400fb781b881dcc2d5c6108
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-09-13 22:55:51 +0430

    add folder stracture

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Running Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is Running On ${PORT}`));
