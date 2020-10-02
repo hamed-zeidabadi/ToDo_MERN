ee3568a5be5256cf74e8fc5276286fc1fdbae3fe
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-02 10:27:26 +0330

    validate data with => Joi

--- server/Controller/User_Controller.js
+++ server/Controller/User_Controller.js
@@ -1,9 +1,21 @@
 const User = require("../Models/User_Model");
 const bcrypt = require("bcryptjs");
+const Joi = require("joi");
 exports.register = async (req, res, next) => {
   try {
     //get requsest data
     const { username, password } = await req.body;
+    // data Validation
+    const Schema = Joi.object({
+      username: Joi.string().min(4).max(255).required(),
+      password: Joi.string().min(6).max(255).required(),
+    });
+    const result = await Schema.validate({
+      username,
+      password,
+    });
+    if (result.error) return res.send(result.error.message);
+
     // hash password
     const salt = await bcrypt.genSalt(10);
     const hashPaswword = await bcrypt.hash(password, salt);
@@ -12,11 +24,10 @@ exports.register = async (req, res, next) => {
       username,
       password: hashPaswword,
     });
-
     await createUser.save();
     res.status(200).send("ok");
     next();
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
