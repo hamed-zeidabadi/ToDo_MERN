24b44d2c090d04a86a9d7d83c83da642b6c6d572
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-02 08:58:36 +0330

    Fix Bug => 'syntax erorr (cors())'

--- server/Routes/User.Router.js
+++ server/Routes/User.Router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../Controller/User_Controller");
 
-module.exports = router.get("/users/login/", (req, res) => {
-  res.send("ok");
+module.exports = router.get("/users", (req, res) => {
+  res.status(200).send("OKKKKKK");
 });
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const app = express();
 // Middleware
 const cors = require("cors");
 const helmet = require("helmet");
-app.use(cors);
-app.use(helmet);
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 require("dotenv").config();
 
@@ -14,8 +14,11 @@ const connectDB = require("./db").connectDB;
 connectDB();
 
 //Routers
-const User_Route = require("./Routes/User.Router");
+const User_Route = require("./Routes/User.Router.js");
 app.use("/api", User_Route);
+app.get("/", (req, res) => {
+  res.status(200).send("ok");
+});
 
 // Running Server
 const PORT = process.env.PORT || 5000;
