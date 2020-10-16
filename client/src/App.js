3680a5a117813e2b2cf548ef4857629126614059
hamed-zeidabadi <zeidabadi73@gmail.com>
2020-10-16 14:19:07 +0330

    add Routing system =>react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Scss/App.scss";
 import Login from "./Pages/Login";
+import Todo from "./Pages/ToDo";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Login />
+      <Router>
+        <Switch>
+          {/* <Route path="/" component={Login} /> */}
+          <Route path="/" component={Todo} />
+          <Redirect path="/" />
+        </Switch>
+      </Router>
     </>
   );
 };
---
+++ client/src/Pages/ToDo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Todo = () => {
+  return (
+    <>
+      <div className="todo">todo</div>
+    </>
+  );
+};
+
+export default Todo;
